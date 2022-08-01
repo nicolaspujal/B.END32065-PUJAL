d11124670e7e9eb280570d38b3844ba6949b5d14
NicolasPujal <nicolaspujal@gmail.com>
2022-08-01 16:27:54 -0300

    desafio 3 - sin completar

---
+++ Clase6/serverHttpExpress/Contenedor.js
@@ -0,0 +1,80 @@
+const {promises:fs} = require('fs');
+
+class Contenedor{
+    constructor(ruta){
+        this.ruta = ruta
+    }
+
+//mismos metodos que en el desafio 2 para ver los productos//guardarlos//borrar uno/todos
+
+    async save(obj){
+        try { 
+        let inProducts = await fs.readFile(this.ruta, 'utf8');
+
+        if(inProducts.lenght == 0){
+            obj.id = 1;
+            const arrayObj = [obj];
+            const objeto = JSON.stringify(arrayObj);
+
+            await fs.appendFile(this.ruta, objeto);
+        } 
+        else{
+            const prodObj = JSON.parse(inProducts);
+            let arrayLength = prodObj.length;
+            obj.id = arrayLength + 1;
+            prodsObj.push(obj);
+            const objeto = JSON.stringify(prodObj);
+
+            await fs.writeFile(this.ruta, objeto);
+        }
+        return console.log(`id : ${obj.id}`);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+    async getAll(obj){
+        try {
+            const products = await fs.readFile(this.ruta, 'utf8');
+            return JSON.parse(products);
+
+        } catch (error) {
+            return [];
+        }
+    }
+
+    async getById(id){
+        const products = await this.getAll();
+        const productById = products.find(p => p.id === id);
+        return productById;
+    }
+
+
+
+//metodos para borrar
+
+    // async deleteById(id){
+    //     const products = await this.getAll();
+
+    //     const newProducts = products.filter(prod => prod.id !== id);
+    //     newProducts.forEach(prod =>{
+    //         if (prod.id > id){
+    //             prod.id --;
+    //         }
+    //     });
+    //     // console.log()
+    //     await fs.writeFile(this.ruta, JSON.stringify(newProducts));
+    // }
+
+    // async deleteAll() {
+    //     const arrayVacio = []
+    //     await fs.writeFile(this.ruta, arrayVacio); //Vacuiamos el array de productos y lo sobreescribimos. 
+    // }
+}
+
+module.exports = Contenedor;
+
+
--- Clase6/serverHttpExpress/server.js
+++ Clase6/serverHttpExpress/server.js
@@ -1,34 +1,43 @@
 const express = require('express');
-
+const { response } = require('express');
 const app = express();
 
-
-// otros metodos aparte de get: post, put(actualizar 'x'), delete(cuando el frontend quiere borrar 'x'), patch(similar al PUT peor menos), options(las opciones que le da el backend para frontend)
-app.get("/", (req, res) => {
-    res.send('<h1 style="color:blue">Bienvenidos al servidor express</h1>');
-  });
-  
-
-let contador = 0;
-
-app.get("/visitas", (req, res) => {
-    res.send(`La cantidad de visitas es ${contador++}`);
-  });
-  
-
-
-const date = new Date();
-app.get("/fyh", (req, res) => {
-  res.send(`la fecha del dia de hoy es ${date.toLocaleString()}`);
+//server donde se abre el puerto
+const server = app.listen(8080, () => {
+    console.log(`El servidor esta escuchando el puerto ${server.address().port}`)
 });
 
 
+//si hay error se muestra en consola
+server.on('error', error => console.log(`Error en el server : ${error}`));
 
-const server = app.listen(8080, ()=>{
-    console.log(`servidor en el puerto ${server.address().port}`);
+app.get('/', (req , res) => {
+    res.send('<h1 style="color:blue;">/productos o /productoRandom</h1>');
+})
+
+//imports
+const Contenedor = require('./Contenedor.js'); //importar la class contenedor
+const product = new Contenedor('./productos.txt');
+
+
+//async function para ver productos
+async function verProductos() {
+    return await product.getAll();
+};
+app.get('/productos', async (req , res) => {
+    res.send(await verProductos());
 });
 
 
-server.on('error', error => console.log(`Error en servidor ${error}`));
+async function productoRandom() {
+    //const de los productos
+    const allProducts = await product.getAll(); 
+    //seleccionar un producto random
+    const random = allProducts[Math.floor(Math.random() * allProducts.length)];
+    //retorna random
+      return random ;
+}
 
-
+app.get('/productoRandom', async (req , res) => {
+    res.send(await productoRandom());
+});
